b8a5d6c0740d983e96a54f1f1d85bcfa975605ca
Kay Wood <kay.wood@findel-education.co.uk>
2019-08-20 17:02:41 +0100

    Add validation setup

--- src/components/FormikWrapper.tsx
+++ src/components/FormikWrapper.tsx
@@ -1,28 +1,68 @@
-import * as React from 'react';
-import { Formik, Form } from 'formik';
- 
+import * as React from "react";
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
+
+// todo
+const styles = {
+  form: {
+    padding: "50px"
+  },
+  fieldset: {
+    border: "none"
+  },
+  input: {
+    padding: "3px 4px"
+  }
+};
+
+const validationSchema: Yup.ObjectSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(1, "This field must be at least 1 character long")
+    .required("This is a required field")
+});
+
 export const FormikWrapper: React.FC = () => (
   <Formik
-    initialValues={{ name: '', email: ''}}
+    initialValues={{ name: "", email: "", password: "" }}
     onSubmit={(values, { setSubmitting }) => {
-      console.log('Submitted value', values);
-      setTimeout(() => setSubmitting(false), 3 * 1000);
+      console.log("Submitted value", values);
+      setTimeout(() => setSubmitting(false), 3000);
     }}
   >
     {props => (
-      <Form>
-        <label htmlFor="email" aria-label="Email">Email</label>
-        <div>
-          <input 
-            id="email" 
-            name="email"
-            type="email"
-            placeholder="Enter your email"
-            value={props.values.email}
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-          />
-        </div>
+      <Form style={styles.form}>
+        <fieldset style={styles.fieldset}>
+          <label htmlFor="email" aria-label="Email">
+            Email
+          </label>
+          <div>
+            <input
+              style={styles.input}
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email"
+              value={props.values.email}
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              required
+            />
+          </div>
+        </fieldset>
+        <fieldset style={styles.fieldset}>
+          <div>
+            <input
+              style={styles.input}
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+              value={props.values.password}
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+            />
+          </div>
+        </fieldset>
       </Form>
     )}
   </Formik>
